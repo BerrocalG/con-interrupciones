03361f3f8ffb4c6cf12b43d7faa0aab39b56b626
BerrocalG <guadalupe.berrocal@usil.pe>
2025-05-05 17:59:27 -0500

    codigo de clase falta configurar el pd2

--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,51 @@
-//tarea
-//codigo sin interrupciones 
 #define F_CPU 16000000UL
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
 
-char canal = 0;
+char channel = 0;  
 
 void select_channel(char channel) {
-    if (channel <= 15) {
-        ADMUX &= 0xF0;      
-        ADMUX |= channel;   
+    if (channel <= 15) {  
+        ADMUX &= 0xF0;       
+        ADMUX |= channel;         
     }
-}
+    }
+
+ISR(INT0_vect) {
+    _delay_ms(50);  
+        channel++;
+        if (channel > 2){
+            channel = 0;
+        }
+        select_channel(channel);  // Siempre actualizar el canal
+    }
+
 
 int main(void) {
-    ADMUX |= (1 << REFS0);               
+    ADMUX |= (1 << REFS0); 
     ADCSRA |= (1 << ADEN) | (1 << ADPS2); 
-    select_channel(0);                   
-    
-    DDRD &= ~0x04;  
-    PORTD |= 0x04;  
-    DDRD |= 0xF8;  
+    select_channel(0);
+    sei();  
+
+    DDRD |= 0xF8;   
     DDRB |= 0x1F;   
 
     char alow = 0;
     char ahigh = 0;
 
     while (1) {
-        // Leer botón con antirrebote
-        if (!(PIND & 0x04)) { 
-            _delay_ms(50); // Debounce
-            if (!(PIND & 0x04)) { 
-                canal++;
-                if (canal > 2) canal = 0;
-                select_channel(canal);
-                while (!(PIND & 0x04)); 
-            }
-        }
 
         ADCSRA |= (1 << ADSC);             
         while (!(ADCSRA & (1 << ADIF)));   
         alow = ADCL;
         ahigh = ADCH;
-        ADCSRA |= (1 << ADIF);           
+        ADCSRA |= (1 << ADIF);             
 
         PORTD &= ~0xF8;
         PORTB &= ~0x1F;
 
-        PORTD |= (alow << 3) & 0xF8;             
-        PORTB |= ((alow >> 5) & 0x07) | ((ahigh << 3) & 0x18);
+        PORTD |= (alow << 3) & 0xF8;  
+        PORTB |= ((alow >> 5) & 0x07) | ((ahigh << 3) & 0x18); 
     }
 }
