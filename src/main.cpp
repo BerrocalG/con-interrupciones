7bafbf1f8e6e7cc69c009cd5583c9c61118a9b24
BerrocalG <guadalupe.berrocal@usil.pe>
2025-05-05 23:19:58 -0500

    interrupcion adc y externa

--- src/main.cpp
+++ src/main.cpp
@@ -1,61 +1,69 @@
 #define F_CPU 16000000UL
-#include <avr/io.h>
+#include <avr/io.h> 
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
-char channel = 0;  
+char channel = 0;  // Canal ADC actual
+char alow = 0;
+char ahigh = 0;
 
-void select_channel(char channel) { //configuracion del adc
-    if (channel <= 15) {  
-        ADMUX &= 0xF0;       
-        ADMUX |= channel;         
+//canal
+void select_channel(char channel) {
+    if (channel <= 15) {
+        ADMUX &= 0xF0;
+        ADMUX |= channel;
     }
-    }
+}
 
-void config_INT0() { //configruar interrupcion
-        EIMSK |= (1 << INT0);    //configurar inetrrupcion
-        EICRA |= (1 << ISC01);    // configurar interrupcion en puerto d
-    
-        DDRD &= ~0x04;      //pin d2 a utilizar
-        PORTD |=0x04;
-}  
-    
-ISR(INT0_vect) { //q haremos  con la interrupcion
+//configuracion de la intercepcion externa
+void config_INT0() {
+    EIMSK |= (1 << INT0);
+    EICRA |= (1 << ISC01);  
+
+    DDRD &= ~0x04;  
+    PORTD |= 0x04;  
+}
+//interrrupcion del pulsador
+ISR(INT0_vect) {
     _delay_ms(50);  
+    if (!(PIND & 0x04)) {
         channel++;
-        if (channel > 2){
+        if (channel > 2) {
             channel = 0;
         }
-        select_channel(channel);  // Siempre actualizar el canal
+        select_channel(channel);  
     }
+}
 
+// interrucpion del adc
+//operaciones
+ISR(ADC_vect) {
+    alow = ADCL;
+    ahigh = ADCH;
+
+    PORTD &= ~0xF8;
+    PORTB &= ~0x1F;
+
+    PORTD |= (alow << 3) & 0xF8;
+    PORTB |= ((alow >> 5) & 0x07) | ((ahigh << 3) & 0x18);
+
+    ADCSRA |= (1 << ADSC);  
+}
 
 int main(void) {
+    // Configuración ADC
+    ADMUX |= (1 << REFS0);  
+    ADCSRA |= (1 << ADEN) | (1 << ADPS2) | (1 << ADIE);  
 
-    ADMUX |= (1 << REFS0); 
-    ADCSRA |= (1 << ADEN) | (1 << ADPS2); 
-    select_channel(0);
-    config_INT0(); //agregar config para que lea las interrupciones
+    DDRD |= 0xF8;  
+    DDRB |= 0x1F; 
+
     sei();  
+    select_channel(0);
+    config_INT0();
+    
+    ADCSRA |= (1 << ADSC);  
 
-    DDRD |= 0xF8;    //los leds
-    DDRB |= 0x1F;   
-
-    char alow = 0; //variable
-    char ahigh = 0;
-
-    while (1) { //el while permanece con lso desplazamientos
-
-        ADCSRA |= (1 << ADSC);             
-        while (!(ADCSRA & (1 << ADIF)));   
-        alow = ADCL;
-        ahigh = ADCH;
-        ADCSRA |= (1 << ADIF);             
-
-        PORTD &= ~0xF8;
-        PORTB &= ~0x1F;
-
-        PORTD |= (alow << 3) & 0xF8;  
-        PORTB |= ((alow >> 5) & 0x07) | ((ahigh << 3) & 0x18); 
+    while (1) {
     }
 }
